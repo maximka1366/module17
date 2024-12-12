62c95bfe3c9dea142fa236f7e9bc507de4d372f7
maximka1366 <maximka66@mail.ru>
2024-12-12 11:03:53 +0300

    Add files via upload

---
+++ module_17_5/app/models/task.py
@@ -0,0 +1,22 @@
+from app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from app.models import __init__
+
+
+class Task(Base):
+    __tablename__ = 'tasks'
+    __table_args__ = {'extend_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    priority = Column(Integer)
+    slug = Column(String, unique=True, index=True)
+    completed = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=True, index=True)
+    user = relationship('User', back_populates='task')
+
+
+#from sqlalchemy.schema import CreateTable
+
+#print(CreateTable(Task.__table__))
---
+++ module_17_5/app/routers/user.py
@@ -0,0 +1,90 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from  app.backend.db_depends import get_db
+from typing import Annotated
+from app.models.user import User
+from app.models.task import  Task
+from sqlalchemy import insert, select, update, delete
+from app.schemas import CreateUser, UpdateUser
+
+from slugify import slugify
+
+router = APIRouter(prefix='/user', tags=['user'])
+
+
+@router.post('/create')
+def create_user(db: Annotated[Session, Depends(get_db)], create_user: CreateUser):
+    existing_user = db.scalar(select(User).where(User.username == create_user.username))
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User with this username already exists")
+
+    db.execute(insert(User).values(username=create_user.username,
+                                   firstname=create_user.firstname,
+                                   lastname=create_user.lastname,
+                                   age=create_user.age,
+                                   slug=slugify(create_user.username)))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
+
+
+
+
+@router.get('/user_id')
+def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is not None:
+        return user
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+
+
+@router.get('/')
+def all_users(db: Annotated[Session, Depends(get_db)]):
+    username = db.scalars(select(User)).all()
+    return username
+
+
+@router.put('/update_user')
+def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, update_user: CreateUser):
+    usernames = db.scalar(select(User).where(User.id == user_id))
+    if usernames is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    db.execute(update(User).where(User.id == user_id).values(
+        firstname=update_user.firstname,
+        lastname=update_user.lastname,
+        age=update_user.age,
+    ))
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+
+
+
+@router.delete('/delete')
+async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user=db.scalar(select(User).where(User.id == user_id))
+    tasks_delete = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found')
+    db.execute(delete(User).where(User.id == user_id))
+
+
+    if tasks_delete:
+        db.execute(delete(Task).where(Task.user_id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
+
+
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    tasks = db.scalars(select(Task).where(User.id == user_id)).all()
+    if tasks is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found')
+    return tasks
